ddcabc9a4770d2ba9712330afac9af135b87e5d9
misbamustaqim <misbamomin@gmail.com>
2017-12-03 01:09:11 -0800

    updating the input file

--- src/tcss543.java
+++ src/tcss543.java
@@ -10,32 +10,32 @@ public class tcss543 {
 	{
 		PushRelabel pushRelabel = new PushRelabel();
 		
-		BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = in.readLine()) != null) {
-                //System.out.println(line);
-                
-                String[] splited = line.split("\\s+");
-                
-                int offset = splited.length - 3;
-                
-                String first = splited[offset + 0] ;
-                String second = splited[offset + 1];
-                int capacity = Integer.parseInt(splited[offset + 2]);  
-                
-                pushRelabel.addEdge(first, second, capacity);
-            }
-        }
-        catch (IOException e) {
-            throw e;
-        }
-        finally {
-            if (in != null) {
-                in.close();
-            }
-        }
+//		BufferedReader in = null;
+//        try {
+//            in = new BufferedReader(new InputStreamReader(System.in));
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                //System.out.println(line);
+//                
+//                String[] splited = line.split("\\s+");
+//                
+//                int offset = splited.length - 3;
+//                
+//                String first = splited[offset + 0] ;
+//                String second = splited[offset + 1];
+//                int capacity = Integer.parseInt(splited[offset + 2]);  
+//                
+//                pushRelabel.addEdge(first, second, capacity);
+//            }
+//        }
+//        catch (IOException e) {
+//            throw e;
+//        }
+//        finally {
+//            if (in != null) {
+//                in.close();
+//            }
+//        }
 		
 //		pushRelabel.addEdge("s", "1", 13);
 //		pushRelabel.addEdge("s", "2", 10);
@@ -50,16 +50,16 @@ public class tcss543 {
 //		pushRelabel.addEdge("2", "t", 2);
 		
 		
-//		pushRelabel.addEdge("s", "1", 16);
-//		pushRelabel.addEdge("s", "2", 13);
-//		pushRelabel.addEdge("1", "2", 10);
-//		pushRelabel.addEdge("2", "1", 4);
-//		pushRelabel.addEdge("1", "3", 12);
-//        pushRelabel.addEdge("2", "4", 14);
-//        pushRelabel.addEdge("3", "2", 9);
-//        pushRelabel.addEdge("3", "t", 20);
-//        pushRelabel.addEdge("4", "3", 7);
-//        pushRelabel.addEdge("4", "t", 4);
+		pushRelabel.addEdge("s", "1", 16);
+		pushRelabel.addEdge("s", "2", 13);
+		pushRelabel.addEdge("1", "2", 10);
+		pushRelabel.addEdge("2", "1", 4);
+		pushRelabel.addEdge("1", "3", 12);
+        pushRelabel.addEdge("2", "4", 14);
+        pushRelabel.addEdge("3", "2", 9);
+        pushRelabel.addEdge("3", "t", 20);
+        pushRelabel.addEdge("4", "3", 7);
+        pushRelabel.addEdge("4", "t", 4);
 		
 		System.out.println(pushRelabel.getMaxFlow());
 	}
